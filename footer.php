b512ecb8bc777bc734836ba95701a144e7108b37
Mutalqureshi <35372795+Mutalqureshi@users.noreply.github.com>
2021-05-10 13:45:01 +0500

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,24 +1,22 @@
-<?php
+<ul class="products">
+	<?php
+		$args = array(
+			'post_type' => 'product',
+			'posts_per_page' => 12
+			);
+		$loop = new WP_Query( $args );
+		if ( $loop->have_posts() ) {
+			while ( $loop->have_posts() ) : $loop->the_post();
+				wc_get_template_part( 'content', 'product' );
+			endwhile;
+		} else {
+			echo __( 'No products found' );
+		}
+		wp_reset_postdata();
+	?>
+</ul>
 
-global $fdata;
-
-?>
-
-<div class="container"><?php
-	if (is_front_page()) {
-	echo " ";
-	}
-	else{
-
-	echo myoutputShortcode(283);
-
-	}
-
-
-  ?></div>
-
-
-
+<!------------------------------------------------------------------------------------------------------------->
 
 <?php if (is_product(332)): ?>
 	
